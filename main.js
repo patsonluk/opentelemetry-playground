28bfacb21d4bf88521a11344e143d3394fcd6129
patsonluk <patson@fullstory.com>
2022-11-16 17:20:11 -0800

    add current OT trace id as x-b3-traceid header, so our FS recorder would report it

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-function ajaxCall(url) {
+const opentelemetry = require("@opentelemetry/api");
+
+
+export function ajaxCall(url) {
   var uid = "test-user-1"
   FS.identify(uid);
   var tracestate = 'fs='+ uid + ":" + FS.getCurrentSessionURL();
-	$.ajax({
-		type: 'GET',
-		url: url,
-                headers: { "tracestate" : tracestate},
-	    success: function(data, status) {
-      alert("Data: " + data + "\nStatus: " + status);
-	},
-       error: function(xhr, ajaxOptions, thrownError) {
-	alert("error: " + xhr.responseText);
-       }
-});
+
+    const tracer = opentelemetry.trace.getTracer(
+        'my-service-tracer'
+    );
+    console.log(tracer)
+    tracer.startActiveSpan('main', span => {
+        const traceId = span.spanContext().traceId
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            headers: { "tracestate" : tracestate, "x-b3-traceid" : traceId},
+            success: function(data, status) {
+                alert("Data: " + data + "\nStatus: " + status);
+                // Be sure to end the span!
+                span.end();
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert("error: " + xhr.responseText);
+                // Be sure to end the span!
+                span.end();
+            }
+        });
+
+    });
+
+
+
 
 }
